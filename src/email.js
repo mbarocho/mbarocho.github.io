b2cf3641045c16eabd728b6c5705c140faeff70c
mbarocho <mbarocho@udel.edu>
2023-09-27 00:41:45 -0400

    Linked .js file and .html file

--- src/email.js
+++ src/email.js
@@ -1,9 +1,11 @@
+emailjs.init("toN7vFxzeanQU3SW5");
+
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
   
     const formData = new FormData(event.target);
   
-    emailjs.sendForm("service_gvoyobe", "template_dgcujsr", formData)
+    emailjs.sendForm("service_rm1yhbp", "template_dgcujsr", formData)
       .then(function(response) {
         console.log("Email sent successfully", response);
       }, function(error) {
