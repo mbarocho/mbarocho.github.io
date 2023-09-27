3f68f04e9541a11372b1a75d66ef39dc7dc8f9be
mbarocho <mbarocho@udel.edu>
2023-09-26 22:19:04 -0400

    Added contact form script for testing

--- src/email.js
+++ src/email.js
@@ -1,4 +1,18 @@
-function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init('YOUR_PUBLIC_KEY');
-})();
+emailjs.init("toN7vFxzeanQU3SW5");
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+  
+    // Get form data
+    const formData = new FormData(event.target);
+  
+    // Send the email
+    emailjs.sendForm("service_gvoyobe", "template_dgcujsr", formData)
+      .then(function(response) {
+        console.log("Email sent successfully", response);
+        // Optionally, you can display a success message to the user.
+      }, function(error) {
+        console.error("Email sending failed", error);
+        // Optionally, you can display an error message to the user.
+      });
+  });
