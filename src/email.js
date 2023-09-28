72376451c3bea297bfbfd68a7849f348d5baa1ab
mbarocho <mbarocho@udel.edu>
2023-09-27 20:33:52 -0400

    Updated code

--- src/email.js
+++ src/email.js
@@ -1,14 +1,15 @@
 emailjs.init("toN7vFxzeanQU3SW5");
 
 document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
+  event.preventDefault();
   
-    const formData = new FormData(event.target);
-  
-    emailjs.sendForm("service_rm1yhbp", "template_dgcujsr", formData)
-      .then(function(response) {
-        console.log("Email sent successfully", response);
-      }, function(error) {
-        console.error("Email sending failed", error);
-      });
-  });
+  const formData = new FormData(event.target);
+
+  emailjs.send("service_rm1yhbp", "template_dgcujsr", formData)
+    .then(function(response) {
+      console.log("Email sent successfully", response);
+    }, function(error) {
+      console.error("Email sending failed", error);
+    });
+});
+
